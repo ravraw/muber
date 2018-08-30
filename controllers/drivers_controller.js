6c8a7136be3af9a85469c908c6120a32a8d1bbd5
ravindra rawat <ravinrawat.wd@gmail.com>
2018-08-30 13:53:52 -0600

    driver edit route and test for it working

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -8,5 +8,22 @@ module.exports = {
     Driver.create(driverProps)
       .then(driver => res.send(driver))
       .catch(next);
+  },
+  editDriver(req, res, next) {
+    const driverId = req.params.id;
+    const driverProps = req.body;
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+      // .then(() => {
+      //   Driver.findById({ _id: driverId });
+      // })
+      .then(driver => res.send(driver))
+      .catch(next);
+  },
+
+  deleteDriver(req, res, next) {
+    const driverId = req.params.id;
+    Driver.findByIdAndRemove({ _id: driverId })
+      .then(res.send("Drive removed"))
+      .catch(next);
   }
 };
--- test/controllers/drivers_controler_test.js
+++ test/controllers/drivers_controler_test.js
@@ -5,7 +5,7 @@ const app = require("../../app");
 const Driver = mongoose.model("driver");
 
 describe("Driver controller", () => {
-  it("makes a POST request to /api/drivers to create a new driver", done => {
+  it("POST  to /api/drivers creates a new driver", done => {
     Driver.countDocuments().then(count => {
       request(app)
         .post("/api/drivers")
@@ -13,6 +13,21 @@ describe("Driver controller", () => {
         .end(() => {
           Driver.countDocuments().then(newCount => {
             assert(count + 1 === newCount);
+            done();
+          });
+        });
+    });
+  });
+
+  it("PUT to /api/drivers/:id  edits an existing driver", done => {
+    const driver = new Driver({ email: "t@t.com", driving: false });
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({ driving: true })
+        .end(() => {
+          Driver.findOne({ email: "t@t.com" }).then(driver => {
+            assert(driver.driving === true);
             done();
           });
         });
